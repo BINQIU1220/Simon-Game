5146d27261b735a660ac11b6f6549d5193c00d1b
BINQIU1220 <ezio12a@hotmail.com>
2024-02-23 15:17:20 +0000

    Show the Sequence to the User with Animations and Sounds

--- game.js
+++ game.js
@@ -4,7 +4,13 @@ let gamePattern = [];
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
+
   let randomChosenColour = buttonColours[randomNumber];
+
   gamePattern.push(randomChosenColour);
-  return gamePattern;
+  $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  let audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+
+  audio.play();
 }
