bbd47a49ed195b9ee428bb77f509a25689e5da96
BINQIU1220 <ezio12a@hotmail.com>
2024-02-24 15:46:54 +0000

    Allow player to start the game by pressing a key

--- game.js
+++ game.js
@@ -4,9 +4,19 @@ let gamePattern = [];
 
 let userClickedPattern = [];
 
+let level = 0;
+
 function playSound(color) {
   let audio = new Audio(`sounds/${color}.mp3`);
   audio.play();
+}
+
+function animatePress(currentColour) {
+  $(`#${currentColour}`).addClass("pressed");
+
+  setTimeout(() => {
+    $(`#${currentColour}`).removeClass("pressed");
+  }, 100);
 }
 
 function nextSequence() {
@@ -19,18 +29,30 @@ function nextSequence() {
   $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
   playSound(randomChosenColour);
+
+  level++;
+
+  $("#level-title").text(`Level ${level}`);
 }
 
 $(document).on("click", ".btn", function (e) {
   let userChosenColour = this.id;
+
   userClickedPattern.push(userChosenColour);
+
   animatePress(userChosenColour);
+
   playSound(userChosenColour);
+
+  if (level !== 0) {
+    setTimeout(() => {
+      nextSequence();
+    }, 1000);
+  }
 });
 
-function animatePress(currentColour) {
-  $(`#${currentColour}`).addClass("pressed");
-  setTimeout(() => {
-    $(`#${currentColour}`).removeClass("pressed");
-  }, 100);
-}
+$(document).keydown(function (e) {
+  if (level === 0) {
+    nextSequence();
+  }
+});
